99d622c8ed7cf3d9086d3f08bef5011f5b267b53
Akramtello <35160721+akram-tello@users.noreply.github.com>
2022-05-27 09:00:38 +0800

    upload app

---
+++ public/core/BackEnd/client/inc/footer.php
@@ -0,0 +1,11 @@
+<footer class="py-5">
+      <div class="container">
+        <div class="row align-items-center justify-content-xl-between">
+          <div class="col-xl-12">
+            <div class="copyright text-center text-xl-left text-muted">
+              © 2019 - <?php echo date('Y');?> Car Rental Management System. Powered By <a href="#" class="font-weight-bold ml-1" target="_blank">Kitkar Developers</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
---
+++ public/core/BackEnd/staff/inc/config.php
@@ -0,0 +1,7 @@
+<?php
+$dbuser="root";
+$dbpass="";
+$host="localhost";
+$db="kitcar_db";
+$mysqli=new mysqli($host,$dbuser, $dbpass, $db);
+?>
